34c6edcb4958ea223ff7eb514d8461d73e2bcb66
jiangpeng <jiangpeng@douban.com>
2015-08-14 14:59:39 +0800

    set apk list

--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,12 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 
 def index(request):
+    apk_list = {'1','2','3'}
+    context = RequestContext(request,{
+        'apk_list' : apk_list,
+    })
     template = loader.get_template('polls/index.html')
-    return HttpResponse(template.render())
+    return HttpResponse(template.render(context))
 
 def detail(request,question_id):
     return HttpResponse("You're looking at question.")
