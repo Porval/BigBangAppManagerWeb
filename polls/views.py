37bdf6c92b63f1580700fceeb3cd6935bca22e72
jiangpeng <jiangpeng@douban.com>
2015-08-14 17:24:32 +0800

    support download apk

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.core.servers.basehttp import FileWrapper
+
 import os
+import mimetypes
 
 def index(request):
     apk_dir = os.path.dirname(__file__) + "/apk"
@@ -21,6 +24,31 @@ def detail(request,question_id):
     return HttpResponse("You're looking at question.")
 
 def apk_detail(request,apk):
-    return HttpResponse(os.path.dirname(__file__) + "/apk/" + apk)
+    file_path = os.path.dirname(__file__) + '/apk/'  + apk
+    original_filename = file_path
+    fp = open(file_path, 'rb')
+    response = HttpResponse(fp.read())
+    fp.close()
+    type, encoding = mimetypes.guess_type(original_filename)
+    if type is None:
+        type = 'application/octet-stream'
+    response['Content-Type'] = type
+    response['Content-Length'] = str(os.stat(file_path).st_size)
+    if encoding is not None:
+        response['Content-Encoding'] = encoding
+        
+    # To inspect details for the below code, see http://greenbytes.de/tech/tc2231/
+    if u'WebKit' in request.META['HTTP_USER_AGENT']:
+        # Safari 3.0 and Chrome 2.0 accepts UTF-8 encoded string directly.
+        filename_header = 'filename=%s' % original_filename.encode('utf-8')
+    elif u'MSIE' in request.META['HTTP_USER_AGENT']:
+        # IE does not support internationalized filename at all.
+        # It can only recognize internationalized URL, so we do the trick via routing rules.
+        filename_header = ''
+    else:
+        # For others like Firefox, we follow RFC2231 (encoding extension in HTTP headers).
+        filename_header = 'filename*=UTF-8\'\'%s' % urllib.quote(original_filename.encode('utf-8'))
 
+    response['Content-Disposition'] = 'attachment; ' + filename_header
+    return response
     # Create your views here.
